05a415793e1d4d8bf03f5497885c949e97bdd456
Kyle <github@fishgate.co.za>
2013-09-26 11:29:58 +0200

    Fixed tracking of cached files.

--- htdocs/wp-config-local-sample.php
+++ htdocs/wp-config-local-sample.php
@@ -8,8 +8,8 @@ define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
  
 // Overwrites the database to save keep edeting the DB
-define('WP_HOME','http://192.168.1.11/OIM/htdocs/');
-define('WP_SITEURL','http://192.168.1.11/OIM/htdocs/');
+define('WP_HOME','http://192.168.1.11/OIM/website-wp/htdocs/');
+define('WP_SITEURL','http://192.168.1.11/OIM/website-wp/htdocs/');
  
 // Turn on debug for local environment
 define('WP_DEBUG', true);
