92d5fba1b40e2265d6b5bd2ecd8fca22421f466d
Kyle <github@fishgate.co.za>
2013-10-03 14:21:27 +0200

    Custom fields on products complete as well as working permalink structure for custom post types.

--- htdocs/wp-content/themes/OIM2013/taxonomy-products_services.php
+++ htdocs/wp-content/themes/OIM2013/taxonomy-products_services.php
@@ -1,18 +1,3 @@
-<?php
-/*
-This is the custom post type taxonomy template.
-If you edit the custom taxonomy name, you've got
-to change the name of this template to
-reflect that name change.
-
-i.e. if your custom taxonomy is called
-register_taxonomy( 'shoes',
-then your single template should be
-taxonomy-shoes.php
-
-*/
-?>
-
 <?php get_header(); ?>
 
 			<div id="content">
