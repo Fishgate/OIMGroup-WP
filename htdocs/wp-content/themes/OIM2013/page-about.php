ac4e2513b11bada5e93096aa1c3693ef9df4c566
Tyrone <ty@crayoncapsule.co.za>
2013-10-07 11:42:39 +0200

    Tyrone commits some local stuff before pulling some important stuff from kyle, namely team page

--- htdocs/wp-content/themes/OIM2013/page-about.php
+++ htdocs/wp-content/themes/OIM2013/page-about.php
@@ -175,10 +175,19 @@ Template Name: About
                                                         </div>
                                                     </div>
                                                     <div class="widget">
-                                                        <h4>Contact Us</h4>
+                                                        <h4 class="widgettitle" id="side-widget-contact">Contact Us</h4>
                                                         <div class="sidebar-form-holder">
                                                             <p>Contact OIM for more information</p>
-                                                            <?php include "contact-form-full.php"; ?>
+                                                            <form id='contact-form'>
+                                                                <input id="name" type="text" data-placeholder="Name*" value="Name*" name="name" />
+                                                                <input id="company" type="text" data-placeholder="Company" value="Company" name="company" />
+                                                                <input id="topic" type="text" data-placeholder="Topic of enquiry*" value="Topic of enquiry*" name="topic" />
+                                                                <input id="number" type="text" data-placeholder="Contact Number" value="Contact Number" name="number" />
+                                                                <input id="email" type="text" data-placeholder="Email Address*" value="Email Address*" name="email" />
+                                                                <textarea id="message" data-placeholder="Message" name="message">Message</textarea>
+                                                                <input type="submit" value="Submit" id="contact-submit" />
+                                                                <br/><br/>* Required fields
+                                                            </form>
                                                         </div>
 
                                                         <a class="sidebar-large-link" href="<?php echo $relpath; ?>contact-us"><img src="<?php echo $relpath; ?>img/sidebars/icon-large-link.jpg" alt=">" />Visit the Contact Us page</a>
