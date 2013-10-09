4ab59bbf6b96ea10edca8431187db3e32e891c9a
Tyrone <ty@crayoncapsule.co.za>
2013-10-09 14:51:00 +0200

    Tyrone finishes up the news single and archives sidebar to be seen to

--- htdocs/wp-content/themes/OIM2013/taxonomy-news_cat.php
+++ htdocs/wp-content/themes/OIM2013/taxonomy-news_cat.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 			<div id="content">
-
+                                
 				<div id="inner-content" class="wrap clearfix">
 
 						<div id="main" class="eightcol first clearfix" role="main">
