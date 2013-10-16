ec0042c94cda3b9f7accfdd7bfa79f8f2a5e20f4
Kyle <github@fishgate.co.za>
2013-10-16 10:37:32 +0200

    Setup archive page for news, finished off more sidebars.

---
+++ htdocs/wp-content/themes/OIM2013/sidebar-clients.php
@@ -0,0 +1,25 @@
+				<div id="sidebar1" class="sidebar fourcol last clearfix" role="complementary">    
+                                        <?php
+
+                                        $ccs = new WP_Query( array('post_type' => 'client_case_studies') );
+
+                                        if($ccs->have_posts()) :
+                                            ?>
+                                    
+                                            <div class="widget">
+                                                <h4 class="widgettitle" id="side-widget-client">Client case studies</h4>
+                                                <ul>
+                                                    <?php
+                                                    while($ccs->have_posts()) : $ccs->the_post();
+                                                        ?>
+                                                            <li><a href="<?php echo get_permalink(get_the_ID()); ?>"><?php echo get_the_title(); ?></a></li>
+                                                        <?php
+                                                    endwhile;
+                                                    ?>
+                                                </ul>
+                                            </div>        
+                                            <?php
+                                        endif;
+
+                                        ?>
+				</div>
