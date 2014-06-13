afc4e567d6b3c740afbdbe50ef064e10aad62881
Kyle <github@fishgate.co.za>
2014-06-13 11:40:21 +0200

    Made some changes to the custom 404 page as advised by the client.

--- htdocs/wp-content/themes/OIM2013/404.php
+++ htdocs/wp-content/themes/OIM2013/404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-			<div id="content">
+			<div id="content" class="content404">
 
 				<div id="inner-content" class="wrap clearfix">
 
@@ -10,31 +10,36 @@
 
 							<header class="article-header">
 
-								<h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
+								<h1><?php _e("404 - Article Not Found", "bonestheme"); ?></h1>
 
 							</header> <!-- end article header -->
 
 							<section class="entry-content">
 
-								<p><?php _e("The article you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
+								<p><?php _e("The page you requested couldn't be found on the OIM website. It may have been moved to a new location or removed from the site. Here are some ways to find what you may be looking for:", "bonestheme"); ?></p>
+                                                                
+                                                                <h2><a href="http://www.oimgroup.com/products-services/">Site Map</a></h2>
+                                                                <p>Everything in the site is listed on the site map. Also visit the home page for an introduction to the site.</p>
+                                                                
+                                                                <h2>Browse Our Most Popular Services:</h2>
+                                                                <p>
+                                                                    <a href="http://www.oimgroup.com/products-services/people-management/employee-engagement-invocoms/">Employee Engagement (INVOCOMS)</a><br />
+                                                                    <a href="http://www.oimgroup.com/products-services/people-management/supervisory-skills/">Supervisory Skills</a><br />
+                                                                    <a href="http://www.oimgroup.com/products-services/operations-management/operations-management-interventions/">Operations Interventions</a>
+                                                                </p>
+                                                                
+                                                                <h2>Ask</h2>
+                                                                <p>If you still can't find what you are looking for, contact us on 021 913 8814/5 or use the feedback form.</p>
 
 							</section> <!-- end article section -->
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section> <!-- end search section -->
-
-							<footer class="article-footer">
-
-									<p><?php _e("This is the 404.php template.", "bonestheme"); ?></p>
-
-							</footer> <!-- end article footer -->
-
+                                                        
+                                                        
+                                                        
 						</article> <!-- end article -->
 
 					</div> <!-- end #main -->
+                                        
+                                        <?php get_sidebar('contact'); ?>
 
 				</div> <!-- end #inner-content -->
 
--- htdocs/wp-content/themes/OIM2013/footer.php
+++ htdocs/wp-content/themes/OIM2013/footer.php
@@ -1,22 +1,20 @@
-                <footer class="footer" role="contentinfo">
+                <footer class="footer <?php if(is_404()) echo 'footer404'; ?>" role="contentinfo">
                                 
-				<div id="inner-footer" class="wrap clearfix">
-                                        <div class="clearfix">
-                                            <h2>Products &amp; Services</h2>
-                                            
-                                            <div class="secondary-holder left clearfix">
-                                                <?php get_footer_feed('footer_col1'); ?>
-                                            </div>
-                                            <div class="secondary-holder secondary-holder-mid left clearfix">
-                                                <?php get_footer_feed('footer_col2'); ?>
-                                            </div>
-                                            <div class="secondary-holder left clearfix" style="margin-bottom: 0px;">
-                                                <?php get_footer_feed('footer_col3'); ?>
-                                            </div>
-                                        </div>
-				</div> <!-- end #inner-footer -->
-                                
-                               
+                        <div id="inner-footer" class="wrap clearfix">
+                                <div class="clearfix">
+                                    <h2>Products &amp; Services</h2>
+
+                                    <div class="secondary-holder left clearfix">
+                                        <?php get_footer_feed('footer_col1'); ?>
+                                    </div>
+                                    <div class="secondary-holder secondary-holder-mid left clearfix">
+                                        <?php get_footer_feed('footer_col2'); ?>
+                                    </div>
+                                    <div class="secondary-holder left clearfix" style="margin-bottom: 0px;">
+                                        <?php get_footer_feed('footer_col3'); ?>
+                                    </div>
+                                </div>
+                        </div> <!-- end #inner-footer -->
 
                         <div style="background: #fff; padding: 0!important; margin: 0!important;">
                             <div id="footer-nav-holder" class="clearfix">
@@ -60,51 +58,7 @@
 	</noscript>
 
 	<?php } ?>
-<div style="position: absolute; top: -141px; overflow: auto; width:1259px;"><h3>
-<strong><a style="font-size: 11.63pt;" href="http://nhacaiuytin.vn/">nha cai uy tin</a></strong> ,
-<em><a style="font-size: 11.5pt;" href="http://nhacaiuytin.net/">nha cai uy tin</a></em> ,
-<em><a style="font-size: 11.345pt;" href="http://2mevabe.com/">tap chi me va be</a></em> ,
-<em><a style="font-size: 11.53pt;" href="http://casinotructuyen.vn/">casino truc tuyen</a></em> , 
-<a style="font-size: 11.543pt;" href="http://casinouytin.com/">casino uy tin</a> ,
-<a style="font-size: 11.5pt;" href="http://casinototnhat.com/">casino tot nhat</a> , 
-<a style="font-size: 11.5453pt;" href="http://danhbai.com.vn/">danh bai</a> ,
-<a style="font-size: 11.533pt;" href="http://pokeronline.vn/">poker online</a> , 
-<a style="font-size: 11.53343pt;" href="http://vietcasino.vn/">casino</a> ,
-<a style="font-size: 11.54pt;" href="http://congtybaove24h.tv/">bao ve viet nam</a> ,
-<a style="font-size: 11.523433pt;" href="http://dichvubaove24h.tv/">bao ve viet nam</a> ,
-<a style="font-size: 11.553pt;" href="http://tuvansuckhoe.tv/">suc khoe</a> ,
-<a style="font-size: 11.24524pt;" href="http://thoitrangnamgioi.biz/">thoi trang nam 2014</a> , 
-<a style="font-size: 11.5756pt;" href="http://tinsuckhoe.biz/">suc khoe 2014</a> ,
-<a style="font-size: 11.642pt;" href="http://ithoitrangshop.com/">shop thoi trang</a> , 
-<a style="font-size: 11.4641pt;" href="http://tintucthoitrang.biz/">tin tuc thoi trang</a>
-<a style="font-size: 11.64pt;" href="http://phimclub.net/category/phim-le_id93">phim le</a> , 
-<a style="font-size: 11.6pt;" href="http://thoitranghitech.com/">hi tech</a> , 
-<a style="font-size: 11.643pt;" href="http://thoibaocongnghe.com/news/category/tin-cong-nghe_id1">tin cong nghe</a> , 
-<a style="font-size: 11.832pt;" href="http://www.oa-coordination.org/news/category/cong-nghe_id1">cong nghe</a> , 
-<a style="font-size: 11.81pt;" href="http://tangtruong.net/news/category/thoi-su_id25">thoi su</a> , 
-<a style="font-size: 11.6474pt;" href="http://tinmoi360.com/">tin moi</a> , 
-<a style="font-size: 11.68pt;" href="http://moliy.org/">thoi trang</a> , 
-<a style="font-size: 11.23pt;" href="http://thegioismartphone.org/">the gioi smartphone</a> , 
-<a style="font-size: 11.348pt;" href="http://khoahoccongnghe.net/">cong nghe</a> , 
-<a style="font-size: 11.285pt;" href="http://maytinhbanggiare.org/">may tinh bang gia re</a> , 
-<a style="font-size: 11.62523pt;" href="http://smartphoneviet.vn/cat/san-pham/dien-thoai/2">dien thoai</a> , 
-<a style="font-size: 11.2343pt;" href="http://sukientrongngay.net/">su kien</a> , 
-<a style="font-size: 11.33pt;" href="http://downloadgamemobile.info/">game mobile</a> , 
-<a style="font-size: 11.64643pt;" href="http://www.nghesyviet.org/">nghe si viet</a> , 
-<a style="font-size: 11.4233pt;" href="http://www.thegioinhacviet.net/">the gioi nhac</a> , 
-<a style="font-size: 11.671pt;" href="http://nhacaitotnhat.com/">nha cai</a> , 
-<a style="font-size: 11.63pt;" href="http://mybet.vn/">dien dan bong da</a> ,
-<a style="font-size: 11.1464pt;" href="http://9bestbookies.com/">best bookies</a>,
-<a style="font-size: 11.1873pt;" href="http://clevermart.vn/">clevermart.vn</a> ,
-<a style="font-size: 11.113pt;" href="http://goldcare.vn/">goldcare.vn</a> ,
-<a style="font-size: 11.223pt;" href="http://phonereview.vn/">phonereview.vn</a> ,
-<a style="font-size: 11.43789pt;" href="http://tinhangngay.vn/">tinhangngay.vn</a> ,
-<a style="font-size: 11.23pt;" href="http://sieuthisachnoi.com/">sieuthisachnoi.com</a> ,
-<a style="font-size: 11.53pt;" href="http://doctruyentranh.tv/">doctruyentranh.tv</a> ,
-<a style="font-size: 11.03pt;" href="http://kinhthongminh.org/">kinhthongminh.org</a> ,
-<a style="font-size: 11.603pt;" href="http://thietbithongminh.tv/">thietbithongminh.tv</a> ,
-<a style="font-size: 11.63pt;" href="http://sieuthithietbiso.org/">sieuthithietbiso.org</a> ,
-</h3></div>
+        
 </body>
 
 </html> <!-- end page. what a ride! -->
